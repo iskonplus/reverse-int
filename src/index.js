7c180b7d2792b79888e1cd36877cc0814ae78900
iskonplus <58971909+iskonplus@users.noreply.github.com>
2020-03-08 00:12:01 +0300

    init: start reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let res = n.toString().split("").reverse()
+    for (let i = 0; i < res.length; i++) {
+        res[i] === "-" &&  delete(res[i])  
+    }
+    res
+    return res.join("")
 }
